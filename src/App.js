f790d479195ad0d62f4bcd19ceabf05bed6a07f4
Kirti Gupta <kirtiigupta12@gmail.com>
2023-06-17 17:08:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Search from "./components/Search";
+import Pagination from "./components/Pagination";
+import Stories from "./components/Stories";
+import "./App.css"
+
+const App = () => {
+  // const data = useGlobalContext();
+  return (
+    <>
+    <Search/>
+    <Pagination/>
+    <Stories/>
+    </>
+    
+  );
+};
+
+export default App
+
+// 40:40 / 2:03:34
